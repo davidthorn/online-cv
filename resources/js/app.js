1ddb99055f6714e29d18221db8bdc3feadd3a252
David Thorn <david.thorn221278@googlemail.com>
2019-04-02 23:33:58 +0200

    added models

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,59 @@
 
+const renderModel = (model, isUl) => {
+    const values = typeof model.value === 'string' ? [model.value] : model.value
+    const tag = isUl === undefined ? 'span' : isUl === true ? 'li' : 'span'
+    const valuesBoxes = values.map(i => {
 
-const loadTemplate = () => {
+        if(typeof i === 'string') {
+            return `<${tag}>${i}</${tag}>`
+        }
+
+        if(typeof i === 'object') {
+            return renderModel(i, model.isUl)
+        }
+        
+    }).join('')
+    return valuesBoxes
+}
+
+
+const loadTemplate = async () => {
 
     const objects = document.querySelectorAll('[template]');
 
     objects.forEach(obj => {
         const templateName = obj.getAttribute('template')
-        fetch(`/templates/${templateName}.html`)
+        const title = obj.getAttribute('title')
+        fetch(`/templates/model.html`)
             .then(function (response) {
                 return response.text();
             })
-            .then(function (myTemplate) {
-                obj.innerHTML = myTemplate
+            .then(async (myTemplate) => {
+                let templateData = `<div class=\"box-title\">${title}</div>`
+                const models = await loadModel(templateName)
+                
+                models.forEach(model => {   
+                    let templateItem = myTemplate.split('{{KEY}}').join(model.key)
+                    templateItem = templateItem.split('{{VALUE}}').join(renderModel(model))
+                    templateData += templateItem
+                })  
+                
+                obj.innerHTML = templateData
+
+
             });
     })
 
-
-
 }
 
+const loadModel = (modelName) => {
+    return fetch(`/models/${modelName}.json`)
+            .then(function (response) {
+                return response.json();
+            })
+}
 
 window.onload = () => {
-    console.log('Hello world');
     loadTemplate()
 }
 
